c0ebb994abb6f6285f1fdc3742f19ead09625568
Deepika <deepikasenthilkumar94@gmail.com>
2020-06-15 18:57:44 +0530

    Training on Python

---
+++ dictDemo.py
@@ -0,0 +1,6 @@
+#Dictinary initializationa and printing
+d = {1:"Deepika", 2:"Eeshwari",3:"Nithy",4:"Kumar"}
+for lookupdic in d:
+    print("The key is {0}".format(lookupdic), end="")
+    print(" and the value of dictionary is {0}".format(d[lookupdic]))
+    
---
+++ forloop.py
@@ -0,0 +1,6 @@
+# printing dictionary
+
+d ={1:"Deepika",2:"Eeshwari",3:"Kumar",4:"Anand"}
+for dic in d:
+    print(dic)
+    print(dic.values())
